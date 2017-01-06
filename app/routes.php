15ccb8167d515892c36907631ed7aac5030c9828
kvfi <kouafi@icloud.com>
2017-01-06 12:18:00 +0000

    add links route

--- app/routes.php
+++ app/routes.php
@@ -17,6 +17,8 @@ $app->get('/', 'HomeController:index')->setName('home');
 $app->get('/Archives', 'App\Controllers\PostController:archives')->setName('archives');
 // $app->get('/contact', 'App\Controllers\PageController:contact')->setName('contact');
 $app->get('/{slug}', 'App\Controllers\PostController:get')->setName('post');
+$app->get('/Links/{year}/{month}', 'App\Controllers\PostController:get')->setName('link');
+
 
 /* TAG */
 $app->get('/tags/{slug}', 'App\Controllers\TagController:get')->setName('tag');

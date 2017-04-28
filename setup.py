1b0c841f9b7005c2d166a7aab5105685581eb567
kvfi <kouafi@icloud.com>
2017-04-28 11:55:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='ouafinet',
+setup(name='kvfi',
 	version='1.0',
 	description='Marginalized lexicon of obssesive science',
 	author='kvfi',
 	author_email='ouafi@ouafi.net',
 	url='https://ouafi.net',
-	packages=['ouafinet'],
+	packages=['kvfi'],
 	install_requires=['flask', 'frontmatter', 'mistune', 'flask_frozen'],
 )

1cd256bbb10275e6cc37ba0bdc93f095d2914d30
kvfi <kouafi@icloud.com>
2017-05-08 10:19:27 +0100

    Change Markdown parser and add HTML extention

--- web.py
+++ web.py
@@ -1,18 +1,13 @@
-import copy, frontmatter, mistune, mdcustom, os
+import copy, frontmatter, os
+import markdown
+from markdown.extensions.wikilinks import WikiLinkExtension
 from skeleton import posts
 from flask import Flask, render_template, Response
 from flask_frozen import Freezer
 
 
 app = Flask(__name__, template_folder="templates")
-app.config.update(dict(
-  PREFERRED_URL_SCHEME = 'https'
-))
-renderer = mdcustom.WikiLinkRenderer()
-inline = mdcustom.WikiLinkInlineLexer(renderer)
-inline.enable_wiki_link()
-
-markdown = mistune.Markdown(renderer, inline=inline)
+app.config.update(dict(PREFERRED_URL_SCHEME = 'https'))
 
 freezer = Freezer(app)
 
@@ -34,14 +29,25 @@ def implodeList(list, sep):
 @app.route("/", endpoint='home')
 def home():
 	headMeta = {'title': 'Ouafi.net', 'description': ''}
-	intro_txt = markdown(frontmatter.load(resources_dir + 'misc/intro.md').content)
+	intro_txt = markdown.markdown(frontmatter.load(resources_dir + 'misc/intro.md').content, extensions=['markdown.extensions.extra', WikiLinkExtension(base_url='https://en.wikipedia.org/wiki/', end_url='')])
 	return render_template('home.html', headMeta=headMeta, intro_txt=intro_txt, posts=posts.Post.get_home())
 
-@app.route("/<slug>", endpoint='post')
+@app.route("/<slug>.html", endpoint='post')
 def post(slug):
 	post = posts.Post.get_post(slug)
 	headMeta = {'title': post['meta']['title'], 'description': post['meta']['subtitle']}
 	return render_template('post.html', headMeta=headMeta, post=posts.Post.get_post(slug))
+
+@app.route("/fr", endpoint='post')
+def post(slug):
+	post = posts.Post.get_post(slug)
+	headMeta = {'title': post['meta']['title'], 'description': post['meta']['subtitle']}
+	return render_template('post.html', headMeta=headMeta, post=posts.Post.get_post(slug))
+
+@app.errorhandler(404)
+def page_not_found(e):
+	headMeta = {'title': 'Page not found, sorry.', 'description': 'Page not found.'}
+	return render_template('404.html', headMeta=headMeta), 404
 
 if __name__ == "__main__":
 	app.run(debug=True)

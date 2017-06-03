3d84a6a94140d7fc8b0224fe1cd298deadeb631d
kvfi <kouafi@icloud.com>
2017-06-03 19:06:04 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kvfi',
+    version='0.1.1',
+    packages=['skeleton'],
+    url='https://github.com/kvfi/kvfi',
+    license='CC0',
+    author='kvfi',
+    author_email='ouafi@ouafi.net',
+    description='A semi-personal journal of thoughts and reflexions.'
+)
